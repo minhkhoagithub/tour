eb797c63ae325767b144aeef6d71117bcc0b6847
minhkhoagithup <22668781.khoa@student.iuh.edu.vn>
2025-09-24 22:17:21 +0700

    fix path 5th

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Hero from '../../components/Hero'
+import Hero from '../../components/Hero.jsx'
 import FeatureDestination from '../../components/FeatureDestination.jsx'
-import Features from '../../components/Features'
-import GalleryComp from '../../components/GalleryComp'
-import Banner from '../..//components/Banner.jsx'
-import Contact from '../../Components/ContactComp'
+import Features from '../../components/Features.jsx'
+import GalleryComp from '../../components/GalleryComp.jsx'
+import Banner from '../../components/Banner.jsx'
+import Contact from '../../components/ContactComp.jsx'
 
 export default function Home () {
     return (
